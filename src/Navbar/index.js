258ad75451cd24d222d96b5606a6eb5229d71537
Thisura Seniya Rathnayake <seniyaratnayake@gmail.com>
2020-10-17 18:33:48 +0530

    Add auto close drawer on mobile

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -3,13 +3,22 @@ import { Layout, Button, Drawer } from "antd";
 import LeftMenu from "./LeftMenu";
 import RightMenu from "./RightMenu";
 import { MenuOutlined } from "@ant-design/icons";
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-
   const showDrawer = () => {
     setVisible(!visible);
   };
+
+  // If you do not want to auto-close the mobile drawer when a path is selected
+  // Delete or comment out the code block below
+  // From here
+  let { pathname: location } = useLocation();
+  useEffect(() => {
+    setVisible(false);
+  }, [location]);
+  // Upto here
 
   return (
     <nav className="navbar">
